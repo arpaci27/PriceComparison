5eb2a1e03c9a009744b538e53d00868f8db4fce0
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-11-23 00:15:18 +0300

    Loader circle added. and driver operations are working on background.

--- PriceComparison/Controllers/PriceFetcher.cs
+++ PriceComparison/Controllers/PriceFetcher.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using PriceComparison.Models;
 
 namespace PriceComparison.Controllers
@@ -10,17 +11,26 @@ namespace PriceComparison.Controllers
         public static (string name, string price, string image) GetProductDetails(string baseUrl, string searchTerm, string siteName)
         {
             string searchUrl = string.Format(baseUrl, searchTerm);
-            IWebDriver driver = new ChromeDriver();
+
+            var options = new ChromeOptions();
+            options.AddArgument("--headless");
+            options.AddArgument("--disable-gpu");
+            options.AddArgument("--no-sandbox");
+            options.AddArgument("--disable-dev-shm-usage");
+            options.AddArgument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+
+            IWebDriver driver = new ChromeDriver(options);
 
             try
             {
                 driver.Navigate().GoToUrl(searchUrl);
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10)); // Explicit wait kullanıyoruz
+
                 var xpaths = XPaths.GetXPaths(siteName, null);
 
-                // Ürün adı, fiyat ve resim URL'si bilgisi alınıyor
-                var productName = driver.FindElement(By.XPath(xpaths[$"{siteName}_name"])).Text;
-                var productPrice = driver.FindElement(By.XPath(xpaths[$"{siteName}_price"])).Text;
-                var productImage = driver.FindElement(By.XPath(xpaths[$"{siteName}_image"])).GetAttribute("src");
+                var productName = wait.Until(drv => drv.FindElement(By.XPath(xpaths[$"{siteName}_name"]))).Text;
+                var productPrice = wait.Until(drv => drv.FindElement(By.XPath(xpaths[$"{siteName}_price"]))).Text;
+                var productImage = wait.Until(drv => drv.FindElement(By.XPath(xpaths[$"{siteName}_image"]))).GetAttribute("src");
 
                 return (productName, productPrice, productImage);
             }
